7a973ec5ff615c14ac241e1bc8a11bd4986b4b99
rahul <r.prahulparihar70@gmail.com>
2025-08-09 18:30:18 +0530

    first commit

---
+++ src/components/Features.jsx
@@ -0,0 +1,130 @@
+import React, { useRef, useState } from 'react';
+import { TiLocationArrow } from 'react-icons/ti';
+
+const BentoTilt = ({children, className = ''}) => {
+
+  const [transformStyle, setTransformStyle] = useState('')
+  const itemRef =  useRef()
+  
+  const handleMouseMove = (e) => {
+    if(!itemRef.current) return
+
+    const {left, top, width, height} =itemRef. current.getBoundingClientRect();
+
+    const relativeX = (e.clientX - left) /width
+    const relativeY = (e.clientY - top) /height
+
+    const tiltX = (relativeY - 0.5) * 5;
+    const tiltY = (relativeX - 0.5) * -5;
+
+    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.95, 0.95, 0.95)`;
+    
+    setTransformStyle(newTransform)
+  }
+
+  const handleMouseLeave = () => {
+    setTransformStyle('')
+  }
+  
+  return (
+    <div className={className} ref={itemRef} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} style={{transform: transformStyle}}> 
+      {children}
+    </div>
+  )
+}
+
+const BentoCard = ({ src, title, description, isComingSoon }) => {
+  return (
+    <div className='relative size-full'>
+      <video
+        src={src}
+        loop
+        muted
+        autoPlay
+        className='absolute top-0 left-0 object-cover object-center size-full'
+      />
+      <div className='relative z-10 flex flex-col justify-between p-5 size-full text-blue-50'>
+        <div>
+          <h1 className='bento-title special-font'>{title}</h1>
+          {description && (
+            <p className='mt-3 text-xs max-w-64 md:text-base'>{description}</p>
+          )}
+        </div>
+      </div>
+      {title}
+    </div>
+  );
+};
+
+const Features = () => {
+  return (
+    <section className='bg-black pb-52'>
+      <div className='container px-3 mx-auto md:px-10'>
+        <div className='px-5 py-32'>
+          <p className='text-lg font-circular-web text-blue-50'>
+            Explore the Zentry Universe
+          </p>
+
+          <p className='max-w-md opacity-50 font-circular-web text-blue-50'>
+            Immerse yourself in an IP-rich product universe where players,
+            agentic AI and blockchain lead the new economic paradigm.
+          </p>
+        </div>
+
+        <BentoTilt className='relative w-full overflow-hidden rounded-md border-hsla mb-7 h-96 md:h-[65vh]'>
+          <BentoCard
+            src='videos/feature-1.mp4'
+            title={
+              <>
+                radia<b>n</b>t
+              </>
+            }
+            description='The game of games app transforming moments across Web2 & Web3 titles into rewards'
+            isComingSoon={true}
+          />
+        </BentoTilt>
+        <div className='grid h-[135vh] grid-cols-2 grid-rows-3 gap-7'> 
+          <BentoTilt className='row-span-1 bento-tilt_1 md:col-span-1 md:row-span-2'>
+            <BentoCard 
+              src='/videos/feature-2.mp4'
+              title={<>zig<b>m</b>a</>}
+              description='The NFT collection merging Zentry’s IP, AI, and gaming—pushing the boundaries of NFT innovation.'
+              
+            />
+          </BentoTilt>
+          <BentoTilt className='row-span-1 bento-tilt_1 ms-32 md:col-span-1 md:ms-0'>
+            <BentoCard 
+              src='videos/feature-3.mp4'
+              title={<>n<b>e</b>xus</>}
+              description='The metagame portal uniting humans & AI to play, compete and earn,shaping profiles that reflect their legacy.'
+            />
+          </BentoTilt>
+          <BentoTilt className='bento-tilt_1 me-14 md:col-span-1 md:me-0'>
+            <BentoCard 
+              src='videos/feature-4.mp4'
+              title={<>az<b>u</b>l</>}
+              description='The agent of agents elevating agentic AI experience to be more fun and productive.'
+            />
+          </BentoTilt>
+          <BentoTilt className='bento-tilt_2'>
+            <div className='flex flex-col justify-between p-5 size-full bg-violet-300'>
+              <h1 className='text-black bento-title special-font max-w-64'>m<b>o</b>re co<b>m</b>ing s<b>o</b>on.</h1>
+              <TiLocationArrow className='m-5 scale-[5] self-end'/>
+            </div>
+          </BentoTilt>
+          <BentoTilt className='bento-tilt_2'>
+            <video 
+              src='/videos/feature-5.mp4'
+              loop
+              muted
+              autoPlay
+              className='object-cover object-center size-full'
+            />
+          </BentoTilt>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,168 @@
+import gsap from "gsap";
+import Button from "./Button";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+import { TiLocationArrow } from "react-icons/ti";
+import { useEffect, useRef, useState } from "react";
+
+gsap.registerPlugin(ScrollTrigger)
+
+const Hero = () => {
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
+
+  const [loading, setLoading] = useState(true);
+  const [loadedVideos, setLoadedVideos] = useState(0);
+
+  const totalVideos = 4 ;
+  const nextVideoRef = useRef(null);
+
+  const handleVideoLoad = () => {
+    setLoadedVideos((prev) => prev + 1);
+  };
+
+  const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+
+  const handleMiniVdClick = () => {
+    setHasClicked(true);
+
+    setCurrentIndex(upcomingVideoIndex);
+  };
+
+  useEffect(() => {
+    if (loadedVideos === totalVideos - 1) {
+      setLoading(false);
+    }
+  }, [loadedVideos]);
+
+  useGSAP(
+    () => {
+      if (hasClicked) {
+        gsap.set("#next-video", { visibility: "visible" });
+        gsap.to("#next-video", {
+          transformOrigin: "center center",
+          scale: 1,
+          width: "100%",
+          height: "100%",
+          duration: 1,
+          ease: "power1.inOut",
+          onStart: () => nextVideoRef.current.play(),
+        });
+        gsap.from("#current-video", {
+          transformOrigin: "center center",
+          scale: 0,
+          duration: 1.5,
+          ease: "power1.inOut",
+        });
+      }
+    },
+    {
+      dependencies: [currentIndex],
+      revertOnUpdate: true,
+    }
+  );
+
+  useGSAP(() => {
+    gsap.set("#video-frame", {
+      clipPath: "polygon(15% 0, 79% 0, 90% 90%, 2% 98%)",
+      borderRadius: "0% 0% 1.5% 1.5%",
+    });
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      borderRadius: "0% 0% 0% 0%",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
+  });
+
+  const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
+
+  return (
+    <div className='relative w-screen overflow-x-hidden h-dvh'>
+      {loading && (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          {/* https://uiverse.io/G4b413l/tidy-walrus-92 */}
+          <div className="three-body">
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+          </div>
+        </div>
+      )}
+
+      <div
+        id='video-frame'
+        className='relative z-10 w-screen overflow-hidden rounded-lg h-dvh bg-blue-75'
+      >
+        <div>
+          <div className='absolute z-50 overflow-hidden rounded-lg cursor-pointer absolute-center mask-clip-path size-64'>
+            <div
+              onClick={handleMiniVdClick}
+              className='transition-all duration-500 ease-in origin-center scale-50 opacity-0 hover:scale-100 hover:opacity-100'
+            >
+              <video
+                ref={nextVideoRef}
+                src={getVideoSrc(currentIndex + 1)}
+                loop
+                muted
+                id='current-video'
+                className='object-cover object-center origin-center scale-150 size-64'
+                onLoadedData={handleVideoLoad}
+              />
+            </div>
+          </div>
+          <video
+            ref={nextVideoRef}
+            src={getVideoSrc(currentIndex)}
+            loop
+            muted
+            id='next-video'
+            className='absolute z-20 invisible object-cover object-center absolute-center size-64'
+            onLoadedData={handleVideoLoad}
+          />
+          <video
+            src={getVideoSrc(
+              currentIndex === totalVideos - 1 ? 1 : currentIndex
+            )}
+            autoPlay
+            loop
+            muted
+            className='absolute top-0 left-0 object-cover object-center size-full'
+            onLoadedData={handleVideoLoad}
+          />
+        </div>
+        <h1 className='absolute z-40 special-font hero-heading bottom-5 right-5 text-blue-75'>
+          G<b>A</b>MING
+        </h1>
+        <div className='absolute top-0 left-0 z-40 size-full'>
+          <div className='px-5 mt-24 sm:px-10'>
+            <h1 className='text-blue-100 special-font hero-heading'>
+              redefi<b>n</b>e
+            </h1>
+
+            <p className='mb-5 text-blue-100 max-w-64 font-robert-regular'>
+              Enter the Metagame Layer <br /> Unleash the Play Economy
+            </p>
+
+            <Button
+              id='watch-trailer'
+              title='Watch trailer'
+              leftIcon={<TiLocationArrow />}
+              containerClass='!bg-yellow-300 flex-center gap-1'
+            />
+          </div>
+        </div>
+      </div>
+      <h1 className='absolute text-black special-font hero-heading bottom-5 right-5'>
+        G<b>A</b>MING
+      </h1>
+    </div>
+  );
+};
+
+export default Hero;
